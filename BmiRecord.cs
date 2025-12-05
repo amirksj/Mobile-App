cec6c6b208ee3996c2d275edbc9585b3c78f23e7
amirksj <amirksj13@yahoo.com>
2025-12-05 16:33:20 +0800

    Create BmiRecord.cs

---
+++ BmiRecord.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IngridDemo3
+{
+    public class BmiRecord
+    {
+        public string DateRecorded { get; set; }
+        public double Weight { get; set; }
+        public double BmiResult { get; set; }
+        public string BmiStatus { get; set; }
+    }
+}
