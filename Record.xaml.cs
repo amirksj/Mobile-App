2c4903abe614859dfa9a0c65cbaf3c8625f86f73
amirksj <amirksj13@yahoo.com>
2025-12-05 16:36:13 +0800

    Create Record.xaml.cs

---
+++ Record.xaml.cs
@@ -0,0 +1,17 @@
+namespace IngridDemo3;
+
+public partial class Record : ContentPage
+{
+    FirebaseHelper firebaseHelper = new FirebaseHelper();
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        displayRecord.ItemsSource = await firebaseHelper.GetAllBmiRecord();
+    }
+    //string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "BMIRecord.txt");
+    public Record()
+	{
+		InitializeComponent();
+        //displayRecord.Text = File.ReadAllText(fileName);
+    }
+}
